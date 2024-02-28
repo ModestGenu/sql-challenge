e557b13780ac756438c5cce59e63b4323a7948b5
ModGenu <mother@rebeccas-mbp.attlocal.net>
2024-02-28 17:43:42 -0600

    last query complete

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Challenge 9
 Here are the resources that I used for this challenge:
 
 https://www.quickdatabasediagrams.com/ to make my database diagram and set up my tables in SQL.
+
+https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-subquery/
+
+Oracle 12c: SQL by Joan Casteel
+
+SQL Quick Start Guide by Walter Shields
+
+
--- SQL_files/emp_queries.sql
+++ SQL_files/emp_queries.sql
@@ -41,10 +41,78 @@ select
 
 select 
 		d.dept_no,
-		e.emp_no, 
+		em.emp_no,
+		e.emp_no,
 		e.last_name,
 		e.first_name,
 		d.dept_name
-	from employees as e
-	inner join departments as d
-	on e.emp_title_id = d.dept_no;
+	from departments as d
+	inner join dept_emp as em
+	on d.dept_no = em.dept_no
+	inner join employees as e
+	on em.emp_no = e.emp_no;
+	
+
+--List first name, last name, and sex of each employee whose first 
+--name is Hercules and whose last name begins with the letter B
+
+select
+		first_name,
+		last_name,
+		sex
+	from employees
+	where first_name = 'Hercules'
+	and last_name like 'B%';
+		
+		
+--List each employee in the Sales department, 
+--including their employee number, last name, and first name 
+
+select 
+	emp_no, last_name, first_name
+from 
+	employees 
+where 
+	emp_no 
+		in (
+			select 
+				emp_no
+			from 
+				dept_emp
+			where
+				dept_no = (
+					select
+						dept_no
+					from 
+						departments
+					where 
+						dept_name = 'Sales'
+			));
+	
+
+--List each employee in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name
+
+select 
+	e.emp_no, 
+	e.last_name, 
+	e.first_name,
+	d.dept_name
+from employees as e
+ 	join dept_emp as de
+		on e.emp_no = de.emp_no
+ 	join departments as d 
+		on de.dept_no = d.dept_no
+			where dept_name in ('Sales', 'Development');
+			
+			
+--List the frequency counts, in descending order, of all the employee last names 
+--(that is, how many employees share each last name)
+
+select count 
+	(last_name), last_name 
+from 
+	employees
+group by 
+	last_name
+order by  count desc;
